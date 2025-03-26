df5b00592ef5d32396a7a378f7aefd1a626cf479
Bruno Evrard <bruno.evrard@gmail.com>
2025-03-26 16:47:26 +0100

    Intégration Firebase

---
+++ Eventorias/Eventorias/EventoriasApp.swift
@@ -0,0 +1,29 @@
+//
+//  EventoriasApp.swift
+//  Eventorias
+//
+//  Created by Bruno Evrard on 26/03/2025.
+//
+
+import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct EventoriasApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
