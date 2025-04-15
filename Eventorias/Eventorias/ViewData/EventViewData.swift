84556d93715377b9044637e5d06fed4254093702
Bruno Evrard <bruno.evrard@gmail.com>
2025-04-15 13:43:23 +0200

    Add Event

--- Eventorias/Eventorias/ViewData/EventViewData.swift
+++ Eventorias/Eventorias/ViewData/EventViewData.swift
@@ -40,3 +40,55 @@ struct EventViewData: Hashable, Decodable {
     }
     
 }
+
+enum Category: String,CaseIterable {
+    
+    case Music
+    case Exhibition
+    case Theater
+    case Concert
+    case Opera
+    case Dance
+    case Festival
+    case Competition
+    case Conference
+    case Tasting
+    
+    var icon: String {
+        switch self {
+        case .football:
+            return "sportscourt"
+        case .natation:
+            return "waveform.path.ecg"
+        case .running:
+            return "figure.run"
+        case .marche:
+            return "figure.walk"
+        case .cyclisme:
+            return "bicycle"
+        case .unknown:
+            return "questionmark"
+        }
+    }
+    
+    init (rawValue: String) {
+        switch rawValue {
+        case "football":
+            self = .football
+        case "natation":
+            self = .natation
+        case "running":
+            self = .running
+        case "marche":
+            self = .marche
+        case "cyclisme":
+            self = .cyclisme
+        default :
+            self = .unknown
+            
+        }
+    }
+    
+   
+}
+
--- Eventorias/Eventorias/Views/Event/AddEventView.swift
+++ Eventorias/Eventorias/Views/Event/AddEventView.swift
@@ -9,11 +9,82 @@ import SwiftUI
 
 struct AddEventView: View {
     @Environment(\.dismiss) var dismiss
+    @State var nom: String = ""
+    
     var body: some View {
         ZStack {
             Color("BackgroundColor")
                 .ignoresSafeArea(.all)
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Title")
+                        .foregroundColor(Color("FontGray"))
+                    TextField("", text: $nom, prompt: Text("New Event ").foregroundColor(Color("FontTextFieldGray")))
+                        .foregroundColor(Color("FontTextFieldGray"))
+                }
+                .padding()
+                .background(Color("BackgroundGray"))
+                .cornerRadius(4)
+                .padding(.horizontal)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Description")
+                        .foregroundColor(Color("FontGray"))
+                    TextField("", text: $nom, prompt: Text("Tap here to enter your description ").foregroundColor(Color("FontTextFieldGray")))
+                        .foregroundColor(Color("FontTextFieldGray"))
+                }
+                .padding()
+                .background(Color("BackgroundGray"))
+                .cornerRadius(4)
+                .padding(.horizontal)
+                
+                HStack {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Date")
+                            .foregroundColor(Color("FontGray"))
+                        TextField(
+                            "",
+                            text: $nom,
+                            prompt: Text(
+                                "MM/DD/YYYY"
+                            ).foregroundColor(Color("FontTextFieldGray"))
+                        )
+                        .foregroundColor(Color("FontTextFieldGray"))
+                    }
+                    .padding()
+                    .background(Color("BackgroundGray"))
+                    .cornerRadius(4)
+                    
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Time")
+                            .foregroundColor(Color("FontGray"))
+                        TextField(
+                            "",
+                            text: $nom,
+                            prompt: Text("HH : MM").foregroundColor(
+                                Color("FontTextFieldGray")
+                            )
+                        )
+                        .foregroundColor(Color("FontTextFieldGray"))
+                    }
+                    .padding()
+                    .background(Color("BackgroundGray"))
+                    .cornerRadius(4)
+                }
+                .padding(.horizontal)
+                  
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Adress")
+                        .foregroundColor(Color("FontGray"))
+                    TextField("", text: $nom, prompt: Text("Enter full address").foregroundColor(Color("FontTextFieldGray")))
+                        .foregroundColor(Color("FontTextFieldGray"))
+                }
+                .padding()
+                .background(Color("BackgroundGray"))
+                .cornerRadius(4)
+                .padding(.horizontal)
+            }
+  
         }
         .navigationBarBackButtonHidden(true)
         .toolbar {
